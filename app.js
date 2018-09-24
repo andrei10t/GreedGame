a9582743c64bd94fb8b6b8fd58d62868ee70314c
andrei.toader1006 <andrei.toader32@gmail.com>
2018-09-24 09:51:46 +0300

    new background, 2 dices, players can choose the winning sum

--- app.js
+++ app.js
@@ -1,22 +1,32 @@
-var scores, roundScore, activePlayer, gamePlaying
+var scores, roundScore, activePlayer, gamePlaying, lastdice
 init()
 
 document.querySelector('.btn-roll').addEventListener('click', function () {
   if (gamePlaying) {
-    var dice = Math.floor(Math.random() * 6) + 1
+    var dice1 = Math.floor(Math.random() * 6) + 1
+    var dice2 = Math.floor(Math.random() * 6) + 1
+    var diceDOM1 = document.getElementById('dice-1')
+    var diceDOM2 = document.getElementById('dice-2')
 
-    var diceDOM = document.querySelector('.dice')
-    diceDOM.style.display = 'block'
-    diceDOM.src = 'Images/dice-' + dice + '.png'
-
-    if (dice !== 1) {
-      roundScore += dice
+    diceDOM1.style.display = 'block'
+    diceDOM2.style.display = 'block'
+    diceDOM1.src = 'Images/dice-' + dice1 + '.png'
+    diceDOM2.src = 'Images/dice-' + dice2 + '.png'
+    /*
+    if (lastdice === 6 && dice === 6) {
+      scores[activePlayer] = 0
+      nextPlayer()
+    }
+    */
+    if (dice1 !== 1 && dice2 !== 1) {
+      roundScore += dice1 + dice2
       document.querySelector(
         '#current-' + activePlayer
       ).textContent = roundScore
     } else {
       nextPlayer()
     }
+    // lastdice = dice
   }
 })
 
@@ -25,9 +35,19 @@ document.querySelector('.btn-hold').addEventListener('click', function () {
     scores[activePlayer] += roundScore
     document.querySelector('#score-' + activePlayer).textContent =
       scores[activePlayer]
-    if (scores[activePlayer] >= 100) {
+
+    var input = document.querySelector('.final-score').value
+    var winScore
+    if (input) {
+      winScore = input
+    } else {
+      winScore = 100
+    }
+
+    if (scores[activePlayer] >= winScore) {
       document.querySelector('#name-' + activePlayer).textContent = 'Winner!'
-      document.querySelector('.dice').style.display = 'none'
+      document.getElementById('dice-1').style.display = 'none'
+      document.getElementById('dice-2').style.display = 'none'
       document
         .querySelector('.player-' + activePlayer + '-panel')
         .classList.add('winner')
@@ -49,7 +69,8 @@ function nextPlayer () {
 
   document.querySelector('.player-0-panel').classList.toggle('active')
   document.querySelector('.player-1-panel').classList.toggle('active')
-  document.querySelector('.dice').style.display = 'none'
+  document.getElementById('dice-1').style.display = 'none'
+  document.getElementById('dice-2').style.display = 'none'
 }
 
 document.querySelector('.btn-new').addEventListener('click', init)
@@ -59,7 +80,8 @@ function init () {
   scores = [0, 0]
   roundScore = 0
   activePlayer = 0
-  document.querySelector('.dice').style.display = 'none'
+  document.getElementById('dice-1').style.display = 'none'
+  document.getElementById('dice-2').style.display = 'none'
   document.getElementById('score-0').textContent = '0'
   document.getElementById('score-1').textContent = '0'
   document.getElementById('current-0').textContent = '0'
@@ -72,4 +94,3 @@ function init () {
   document.querySelector('.player-1-panel').classList.remove('winner')
   document.querySelector('.player-0-panel').classList.add('active')
 }
-
